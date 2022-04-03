6eb1a0475e663c11e82bb46b208934ca5bbc0d55
Resshin <97952141+55F@users.noreply.github.com>
2022-04-03 12:24:41 +0530

    Add files via upload

---
+++ config/__init__.py
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2021-2022 by TeamMarin@Github, < https://github.com/TeamMarin >.
+#
+# This file is part of < https://github.com/TeamMarin/MarinMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamMarin/MarinMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from .config import *
